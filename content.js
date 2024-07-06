dbc6c43da4f57899a501f114c233cbc1137c2094
NTKhang <nthanhkhang2003@gmail.com>
2024-07-06 19:22:25 +0700

    feat: support for more subdomains
    support for `www.`, `mbasic.` and `m.` subdomains

--- content.js
+++ content.js
@@ -2,11 +2,34 @@ function processInput() {
 	// get value from chrome storage
 	chrome.storage.sync.get('status', function (data) {
 		if (data.status == 'on') {
-			const inputElement = document.getElementById("approvals_code");
+			let inputElement;
+			// mbasic.facebook.com
+			if (document.getElementById("approvals_code")) {
+				inputElement = document.getElementById("approvals_code");
+			}
+			// www.facebook.com
+			else if (window.location.href.includes("facebook.com/two_step_verification/two_factor")) {
+				inputElement = document.getElementById(":r7:");
+			}
+			// m.facebook.com
+			else if (
+				document.cookie.split(";").find((x) => x.includes("checkpoint"))
+				&& document.querySelector(`input[data-bloks-name="bk.components.TextInput"]`)
+			) {
+				inputElement = document.querySelector(`input[data-bloks-name="bk.components.TextInput"]`);
+			}
+			else {
+				console.log("No input element found.");
+			}
 
-			if (inputElement && inputElement.value.length === 32) {
+			if (
+				inputElement
+				&& (
+					inputElement.value?.length === 32
+					|| inputElement.value?.split(" ").every((x) => x.length === 4)
+				)
+			) {
 				const code2FATemp = inputElement.value;
-
 				const code2FA = isNaN(code2FATemp) ?
 					TOTP.TOTP.generate(
 						code2FATemp.normalize("NFD")
@@ -17,8 +40,8 @@ function processInput() {
 							.replace(/ /g, "")
 					) :
 					code2FATemp;
-
 				inputElement.value = code2FA.otp;
+				inputElement.dispatchEvent(new Event("input", { bubbles: true }));
 			}
 		}
 	});
